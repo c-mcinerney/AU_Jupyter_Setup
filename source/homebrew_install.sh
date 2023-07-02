3affe2ca3cd4127b2769ff9ae927540872f727e9
Chris McInerney <40616093+c-mcinerney@users.noreply.github.com>
2023-07-02 18:08:34 -0400

    Update homebrew_install.sh

--- source/homebrew_install.sh
+++ source/homebrew_install.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 ## Install Homebrew - requires system root password - press return to install the defaults
 export HOMEBREW_NO_INSTALL_FROM_API=1
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
